fff225fd3668e5569680d208003ee7ff49ef4b90
lmuenter <50193931+lmuenter@users.noreply.github.com>
2021-01-15 17:13:55 +0100

    prepared clustering and gene extraction

---
+++ R/genes.R
@@ -0,0 +1,11 @@
+###########################
+#
+# Extract Genes from ORA
+#
+###########################
+
+# purpose: A hacky solution to obtain genes associated to GOterms in PANTHER output.
+# date: 15.01.2021
+# author: Lukas Muenter
+# note: only tested for Arabidopsis thaliana!
+
---
+++ R/oraclust.R
@@ -0,0 +1,11 @@
+#############################
+#
+# GOterm Kmedoids clustering
+#
+#############################
+
+# purpose: Cluster GOterms based on shared reads
+# date: 15.01.2021
+# author: Lukas Muenter
+# note: only tested for Arabidopsis thaliana!
+
