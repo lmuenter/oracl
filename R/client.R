03a7963046cf517558cfcb103db5ad0bfa9a1ecd
lmuenter <50193931+lmuenter@users.noreply.github.com>
2021-01-15 13:49:34 +0100

    added file "client"

---
+++ R/client.R
@@ -0,0 +1,11 @@
+###########################
+#
+# PANTHER client
+#
+###########################
+
+# purpose: simple client for sending a geneset and a background geneset to panther.org for an overrepresentation test
+# date: 15.01.2021
+# author: Lukas Muenter
+# note: only tested for Arabidopsis thaliana!
+
