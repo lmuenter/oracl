06aa8eddcaaa7cffd3be473a36c02719f5c9586e
lmuenter <50193931+lmuenter@users.noreply.github.com>
2021-07-23 12:27:30 +0200

    added sample datasets

--- data-raw/datasets-goterm.R
+++ data-raw/datasets-goterm.R
@@ -4,8 +4,28 @@
 #
 ###############################
 
+## GO-Term datasets
 bp.df = read.csv("data/goterms/go_bp_ath.csv", stringsAsFactors = FALSE)
 mf.df = read.csv("data/goterms/go_mf_ath.csv", stringsAsFactors = FALSE)
 cc.df = read.csv("data/goterms/go_cc_ath.csv", stringsAsFactors = FALSE)
 
-usethis::use_data(bp.df, mf.df, cc.df, internal = TRUE)
+## sample datasets
+GS01 = readLines("data/genesets/GS01.txt")
+GS02 = readLines("data/genesets/GS02.txt")
+GS03 = readLines("data/genesets/GS03.txt")
+GS04 = readLines("data/genesets/GS04.txt")
+GS05 = readLines("data/genesets/GS05.txt")
+background = readLines("data/background/background.txt")
+
+## export
+usethis::use_data(GS01,
+                  GS02,
+                  GS03,
+                  GS04,
+                  GS05,
+                  background,
+                  bp.df,
+                  mf.df,
+                  cc.df,
+                  internal = TRUE,
+                  overwrite = TRUE)
